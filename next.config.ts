5e11f8c59ea7cb194f3885172c3f16dc9ba5808c
talisaineskye-lang <talisaine.skye@gmail.com>
2026-01-28 13:26:29 -0700

    push fix for youtube thumbnails

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.ytimg.com",
+        hostname: "*.ytimg.com",
       },
       {
         protocol: "https",
